70f1f36db5329ca489500aa7a5201837d337ab24
Tyler <ttrammell98@gmail.com>
2021-11-10 20:00:48 -0600

    Testing

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api', (req, res) => {
-  res.json({message: 'Hello World!' });
+  res.json({message: 'Welcome!' });
 });
 
 app.listen(port, () => {
